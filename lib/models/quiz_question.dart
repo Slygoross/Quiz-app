dbce089f805d3a3cda52f62bac3f1d8adf234fe0
Pranav Modgil <pranavmodgil@gmail.com>
2024-02-24 23:25:47 +0530

    Video 67 Done

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,6 @@
+class quizQuestion {
+  const quizQuestion(this.text, this.answers);
+
+  final String text;
+  final List<String> answers;
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,13 +12,101 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const GradientText(
-      text: "Questions Screen",
-      gradient: LinearGradient(colors: [
-        Color.fromRGBO(212, 20, 90, 1),
-        Color.fromRGBO(251, 176, 59, 1),
-      ]),
-      style: TextStyle(fontSize: 35.0),
+    // return const GradientText(
+    //   text: "Questions Screen",
+    //   gradient: LinearGradient(colors: [
+    //     Color.fromRGBO(212, 20, 90, 1),
+    //     Color.fromRGBO(251, 176, 59, 1),
+    //   ]),
+    //   style: TextStyle(fontSize: 35.0),
+    // );
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const GradientText(
+            text: "Questions Screen",
+            gradient: LinearGradient(colors: [
+              Color.fromRGBO(212, 20, 90, 1),
+              Color.fromRGBO(251, 176, 59, 1),
+            ]),
+            style: TextStyle(fontSize: 35.0),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.transparent,
+            ),
+            child: const GradientText(
+              text: 'Answer 1',
+              gradient: LinearGradient(colors: [
+                Color.fromRGBO(212, 20, 90, 1),
+                Color.fromRGBO(251, 176, 59, 1),
+              ]),
+              style: TextStyle(fontSize: 35.0),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.transparent,
+            ),
+            child: const GradientText(
+              text: 'Answer 2',
+              gradient: LinearGradient(colors: [
+                Color.fromRGBO(212, 20, 90, 1),
+                Color.fromRGBO(251, 176, 59, 1),
+              ]),
+              style: TextStyle(fontSize: 35.0),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.transparent,
+            ),
+            child: const GradientText(
+              text: 'Answer 3',
+              gradient: LinearGradient(colors: [
+                Color.fromRGBO(212, 20, 90, 1),
+                Color.fromRGBO(251, 176, 59, 1),
+              ]),
+              style: TextStyle(fontSize: 35.0),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.transparent,
+            ),
+            child: const GradientText(
+              text: 'Answer 4',
+              gradient: LinearGradient(colors: [
+                Color.fromRGBO(212, 20, 90, 1),
+                Color.fromRGBO(251, 176, 59, 1),
+              ]),
+              style: TextStyle(fontSize: 35.0),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
