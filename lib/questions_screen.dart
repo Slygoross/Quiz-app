0479d329493f7e6c9696d201c152d475b599b98d
Pranav Modgil <pranavmodgil@gmail.com>
2024-02-26 12:04:48 +0530

    Video 69 Done

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:adv_basics/answer_button.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/gradient_text.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -21,45 +22,47 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     //   ]),
     //   style: TextStyle(fontSize: 35.0),
     // );
+
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const GradientText(
-            text: "Questions Screen",
-            gradient: LinearGradient(colors: [
+          GradientText(
+            text: currentQuestion.text,
+            gradient: const LinearGradient(colors: [
               Color.fromRGBO(212, 20, 90, 1),
               Color.fromRGBO(251, 176, 59, 1),
             ]),
-            style: TextStyle(fontSize: 35.0),
+            style: const TextStyle(fontSize: 35.0),
           ),
           const SizedBox(
             height: 30,
           ),
           AnswerButton(
-            answerText: 'Answer 1',
+            answerText: currentQuestion.answers[0],
             onTap: () {},
           ),
           const SizedBox(
             height: 30,
           ),
           AnswerButton(
-            answerText: 'Answer 2',
+            answerText: currentQuestion.answers[1],
             onTap: () {},
           ),
           const SizedBox(
             height: 30,
           ),
           AnswerButton(
-            answerText: 'Answer 3',
+            answerText: currentQuestion.answers[2],
             onTap: () {},
           ),
           const SizedBox(
             height: 30,
           ),
           AnswerButton(
-            answerText: 'Answer 4',
+            answerText: currentQuestion.answers[3],
             onTap: () {},
           ),
         ],
