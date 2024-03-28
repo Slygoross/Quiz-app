7cf0235b12810a2a6da261522cc039171be63439
Pranav Modgil <pranavmodgil@gmail.com>
2024-03-29 01:36:23 +0530

    Exception Updation

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -11,19 +11,24 @@ class QuestionsSummary extends StatelessWidget {
         (data) {
           return Row(
             children: [
-              Text(((data['question_index'] as int) + 1).toString()),
-              Column(
-                children: [
-                  Text((data['question'] as String),
-                      style: GoogleFonts.lato(color: Colors.white)),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text((data['user_answer'] as String),
-                      style: GoogleFonts.lato(color: Colors.white)),
-                  Text((data['correct_answer'] as String),
-                      style: GoogleFonts.lato(color: Colors.white)),
-                ],
+              Text(
+                ((data['question_index'] as int) + 1).toString(),
+                style: GoogleFonts.lato(color: Colors.red),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text((data['question'] as String),
+                        style: GoogleFonts.lato(color: Colors.white)),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text((data['chosen_answer'] as String),
+                        style: GoogleFonts.lato(color: Colors.white)),
+                    Text((data['correct_answer'] as String),
+                        style: GoogleFonts.lato(color: Colors.white)),
+                  ],
+                ),
               )
             ],
           );
