99065659d524d2077f48ab56104c82fe50269716
Pranav Modgil <pranavmodgil@gmail.com>
2024-03-29 01:51:11 +0530

    Video 87

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -6,34 +6,39 @@ class QuestionsSummary extends StatelessWidget {
   final List<Map<String, Object>> summaryData;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map(
-        (data) {
-          return Row(
-            children: [
-              Text(
-                ((data['question_index'] as int) + 1).toString(),
-                style: GoogleFonts.lato(color: Colors.red),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text((data['question'] as String),
-                        style: GoogleFonts.lato(color: Colors.white)),
-                    const SizedBox(
-                      height: 5,
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return Row(
+                children: [
+                  Text(
+                    ((data['question_index'] as int) + 1).toString(),
+                    style: GoogleFonts.lato(color: Colors.red),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text((data['question'] as String),
+                            style: GoogleFonts.lato(color: Colors.white)),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text((data['chosen_answer'] as String),
+                            style: GoogleFonts.lato(color: Colors.white)),
+                        Text((data['correct_answer'] as String),
+                            style: GoogleFonts.lato(color: Colors.white)),
+                      ],
                     ),
-                    Text((data['chosen_answer'] as String),
-                        style: GoogleFonts.lato(color: Colors.white)),
-                    Text((data['correct_answer'] as String),
-                        style: GoogleFonts.lato(color: Colors.white)),
-                  ],
-                ),
-              )
-            ],
-          );
-        },
-      ).toList(),
+                  )
+                ],
+              );
+            },
+          ).toList(),
+        ),
+      ),
     );
   }
 }
